160441b178f76ecae67985fb51bf86464e7de01e
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2022-12-07 22:09:28 -0300

    Vercel deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 const products = require('./db.json');
 
@@ -10,10 +10,12 @@ app.get('/products/GBP', (req, res)=>{
     res.status(200).json(products.GBP);
 });
 
-app.use('/images', express.static('images'))
+app.use('/images', express.static('images'));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log("OK")
+    console.log("Running on port 5000.");
 });
+
+module.exports = app;
